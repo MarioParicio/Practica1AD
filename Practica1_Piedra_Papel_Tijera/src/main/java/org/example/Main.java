f58c7ffc4642d0b44eb4781ffa52faf8ff5b17d1
Mario Paricio <mpariciob@gmail.com>
2022-10-07 20:35:10 +0200

    Idioma, slf4j y log implementado

--- Practica1_Piedra_Papel_Tijera/src/main/java/org/example/Main.java
+++ Practica1_Piedra_Papel_Tijera/src/main/java/org/example/Main.java
@@ -1,8 +1,71 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+import java.io.*;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+public class Main {
+
+    static FileReader reader;
+
+    static ResourceBundle currentBundle;
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        log.trace("Starting " );
+        log.error("error");
+
+
+        //default locale
+        ResourceBundle bundle = ResourceBundle.getBundle("ApplicationMessages");
+        log.trace("Trace message");
+        log.info("Log Test");
+        ResourceBundle bundleEN = ResourceBundle.getBundle("ApplicationMessages_us_EN", Locale.US);
+        currentBundle = bundle;
+
+
+        //lets print some messages
+        println("CountryName");
+        print("CurrencyCode");
+        try {
+            reader = new FileReader("config/config.properties");
+            Properties properties = new Properties();
+            properties.load(reader);
+            String dificultad = properties.getProperty("difficulty");
+            System.out.println(dificultad);
+
+            properties.setProperty("capullo", "melon");
+            properties.store(new BufferedWriter(new FileWriter("config/config.properties")), "Ejemplo");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+            throw new RuntimeException(e);
+        } finally {
+            reader.close();
+        }
     }
+
+    private static void println(String key) {
+
+        System.out.println(currentBundle.getString(key));
+
+    }
+    private static void print(String key) {
+
+        System.out.println(currentBundle.getString(key));
+
+    }
+
 }
